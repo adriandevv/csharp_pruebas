82febbd3899070ff14559d1ecd01eb1646f87420
Adrian Valencia Hernandez <adrian15996@gmail.com>
2025-02-03 16:48:54 -0600

    initial commit of C# project with basic structure and hello world example

---
+++ pruebas/Program.cs
@@ -0,0 +1,15 @@
+﻿void sayHello() {
+    Console.WriteLine("Hello, World!");
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    sayHello();
+    Console.WriteLine(add(1, 2));
+    return 0;
+}
+
+main();
